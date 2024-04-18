c2e2e447c515145209e2b92015df3fff7e2ee9e0
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2024-04-19 00:06:20 +0200

    Praca -> following instructions

--- src/main/java/com/sportclub/model/Zespol.java
+++ src/main/java/com/sportclub/model/Zespol.java
@@ -6,17 +6,21 @@ import java.util.List;
 public class Zespol {
     private String nazwa;
     private Manager manager;
+    private Praca praca;
     private List<Pracownik> pracownicy;
+
 
     public Zespol(String nazwa, Manager manager) {
         this.nazwa = nazwa;
         this.manager = manager;
         this.pracownicy = new ArrayList<>();
+        manager.dodajZespol(this);
     }
 
     public void dodajPracownika(Pracownik pracownik) {
         if (!(pracownik instanceof Manager)) {
             pracownicy.add(pracownik);
+            pracownik.dodajZespol(this);
             System.out.println("Pracownik " + pracownik.getImie() + " " + pracownik.getNazwisko() + " dodany do zespołu " + nazwa);
         } else {
             System.out.println("Managerowie nie mogą być dodani jako zwykli pracownicy do zespołu.");
@@ -27,6 +31,14 @@ public class Zespol {
         for (Pracownik pracownik : nowiPracownicy) {
             dodajPracownika(pracownik);
         }
+    }
+
+    public void ustawPrace(Praca praca){
+        this.praca = praca;
+    }
+
+    public Praca getPraca() {
+        return praca;
     }
 
     public String getNazwa() {
